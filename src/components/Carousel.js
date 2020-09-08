23a1d84355e5987a3cd0bdd10263a312eaf5bbed
Nathan P <67067343+SeptumPrince@users.noreply.github.com>
2020-09-08 19:19:21 -0400

    Added Carousel Component

---
+++ src/components/Carousel.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>Carousel</h1>;
+  }
+}
+
+export default Carousel;
