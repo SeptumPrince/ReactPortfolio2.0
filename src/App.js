433413f4386de68e2f19f81f4a2338b904385050
Nathan P <67067343+SeptumPrince@users.noreply.github.com>
2020-09-08 19:57:24 -0400

    Added index.css

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
       <Router>
         <Container className="p-0" fluid={true}>
           <Navbar className="border-bottom" bg="transparent" expand="lg">
-            <Navbar.Brand>Garrett Love</Navbar.Brand>
+            <Navbar.Brand>Septum Prince </Navbar.Brand>
 
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import CardInfo from "./CardInfo";
+
 function Card(props) {
   return (
     <div>
       <img className="" src={props.item.imgSrc} alt={props.item.title} />
+      {props.item.selected && (
+        <CardInfo
+          title={props.item.title}
+          subTitle={props.item.subTitle}
+          link={props.item.linki}
+        />
+      )}
     </div>
   );
 }
