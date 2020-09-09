fe9044cc37c14f903c4840027668489f2701deed
Nathan P <67067343+SeptumPrince@users.noreply.github.com>
2020-09-08 20:28:12 -0400

    Turned contact page into component

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-function ContactPage() {
-  return <h1>ContactPage</h1>;
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      disabled: false,
+      emailSent: null
+    };
+  }
+  render() {
+    return <h1>ContactPage</h1>;
+  }
 }
 
 export default ContactPage;
