bbf8dedc94e38a104f9ae235edb7d4a17d418a23
Nathan P <67067343+SeptumPrince@users.noreply.github.com>
2020-09-08 20:01:17 -0400

    Added classnames to elements on cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,11 @@ import CardInfo from "./CardInfo";
 function Card(props) {
   return (
     <div>
-      <img className="" src={props.item.imgSrc} alt={props.item.title} />
+      <img
+        className="n-inline-block n-card"
+        src={props.item.imgSrc}
+        alt={props.item.title}
+      />
       {props.item.selected && (
         <CardInfo
           title={props.item.title}
--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -5,9 +5,9 @@ function CardInfo(props) {
   const style = useSpring({ opacity: 1, from: { opcity: 0 } });
 
   return (
-    <animated.div className="" style={style}>
-      <p>{props.title}</p>
-      <p>{props.subTitle}</p>
+    <animated.div className="n-card-info" style={style}>
+      <p className="n-card-title">{props.title}</p>
+      <p className="n-card-subT itle">{props.subTitle}</p>
       <a href={props.link} target="_blank">
         View
       </a>
