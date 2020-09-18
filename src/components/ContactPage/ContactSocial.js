ec322e5668d97f1a5726ae95636701e690d0cb71
Nathan Pecchia <septumprince@MacBook-Pro-2.local>
2020-09-18 12:48:55 -0400

    Added social buttons to contact page

--- src/components/ContactPage/ContactSocial.js
+++ src/components/ContactPage/ContactSocial.js
@@ -1,7 +1,21 @@
 import React from "react";
 import Card from 'react-bootstrap/Card'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons"
 export default function ContactSocial() {
 
-  return <Card.Body><h1>Social</h1></Card.Body>
+  return <Card.Body>
+    <a href="https://www.linkedin.com/in/nathan-pecchia/" target="_blank">
+    <FontAwesomeIcon icon={faLinkedin} />
+    </a>
+    <a href="https://github.com/SeptumPrince" target="_blank">
+    <FontAwesomeIcon icon={faGithubSquare} />
+    </a>
+    <a href="https://www.instagram.com/nathanrobertpecchia/" target="_blank">
+    <FontAwesomeIcon icon={faInstagram} />
+    </a>
+
+  </Card.Body>
 }
