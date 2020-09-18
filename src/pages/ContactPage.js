efe2d61fe7ce8c58fc0caea47976359a1dff7003
Nathan Pecchia <septumprince@MacBook-Pro-2.local>
2020-09-18 12:20:13 -0400

    Pulled Contact Form out to be own component

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -2,10 +2,13 @@ import React from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 import Hero from "../components/Hero";
 import Content from "../components/Content";
 import Axios from "axios";
+
+import ContactForm from "../components/ContactForm"
 
 class ContactPage extends React.Component {
   constructor(props) {
@@ -67,59 +70,31 @@ class ContactPage extends React.Component {
       <div className="page">
         <Hero title={this.props.title} />
 
-        <Content>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group>
-              <Form.Label htmlFor="full-name">Full Name</Form.Label>
-              <Form.Control
-                id="full-name"
-                name="name"
-                type="text"
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
 
-            <Form.Group>
-              <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control
-                id="email"
-                name="email"
-                type="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
+        <Accordion>
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+        Click me!
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="0">
+      <Card.Body>Hello! I'm the body</Card.Body>
+    </Accordion.Collapse>
+  </Card>
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="link" eventKey="1">
+        Click me!
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="1">
+      <Card.Body><ContactForm /></Card.Body>
+    </Accordion.Collapse>
+  </Card>
+</Accordion>
 
-            <Form.Group>
-              <Form.Label htmlFor="message">Message</Form.Label>
-              <Form.Control
-                id="message"
-                name="message"
-                as="textarea"
-                rows="3"
-                value={this.state.message}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
 
-            <Button
-              className="d-inline-block"
-              variant="primary"
-              type="submit"
-              disabled={this.state.disabled}
-            >
-              Send
-            </Button>
-
-            {this.state.emailSent === true && (
-              <p className="d-inline success-msg">Email Sent</p>
-            )}
-            {this.state.emailSent === false && (
-              <p className="d-inline err-msg">Email Not Sent</p>
-            )}
-          </Form>
-        </Content>
       </div>
     );
   }
