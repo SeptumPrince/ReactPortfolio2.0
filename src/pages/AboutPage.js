77bd60d46d2349a843eacdd859d593dca65533c7
Nathan Pecchia <septumprince@MacBook-Pro-2.local>
2020-09-22 14:34:12 -0400

    Updated ABOUT PAGE bio

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,13 +8,11 @@ function AboutPage(props) {
       <Hero title={props.title} />
 
       <Content>
-        <p>LOREM.</p>
-
-        <p>LOREM.</p>
-
-        <p>LOREMS</p>
-
-        <p>LOREM</p>
+      <p>Web developer from an NYC performing artist's background. </p>
+      <p> Full-stack skills include a specialty in React.js and a certificate through Columbia University. </p>
+      <p>Having worked simultaneous odd jobs for several years, I’m experienced in balancing (and seamlessly switching between) skill sets and professional personas. </p>
+      <p>Recent projects include an app to help New Yorkers find COVID-19 testing/charity resources and a blood sugar tracking app for patients with Type 2 Diabetes. </p>
+      <p>Considering current events, I'm motivated to sharpen my skills and work for a company that is doing important work that contributes to the betterment of our communities - particularly <b>food insecurity, climate change and social justice.</b> </p>
       </Content>
     </div>
   );
