099daf65b085949947aaec5c8b159c07228effbf
Nathan Pecchia <septumprince@macbook-pro-2.home>
2020-10-15 12:39:03 -0400

    Fixed Card text

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,30 +4,32 @@ import CardInfo from "./CardInfo";
 
 function ProjectCard(props) {
   return (
-    <div
-      className="n-inline-block n-card"
+    <Card
+      className="flex-row n-card"
       onClick={(e) => props.click(props.item)}
     >
+        <img
+        className="n-card-image"
+        variant="top"
+        src={props.item.imgSrc}
+        alt={props.item.title}
+      />
       {props.item.selected && (
         <Card.Body>
           <Card.Text>
-            <CardInfo
-              title={props.item.title}
-              subTitle={props.item.subTitle}
-              link={props.item.link}
-            />
+          <CardInfo
+            title={props.item.title}
+            subTitle={props.item.subTitle}
+            link={props.item.link}
+          />
           </Card.Text>
           <Card.Link href={props.item.deploy_link} target="_blank">Deployed</Card.Link>
           <Card.Link href={props.item.git_link} target="_blank">Repo</Card.Link>
         </Card.Body>
    )}
-      <img
-        className="n-card-image"
-        src={props.item.imgSrc}
-        alt={props.item.title}
-      />
+
       
-    </div>
+    </Card>
   );
 }
 
